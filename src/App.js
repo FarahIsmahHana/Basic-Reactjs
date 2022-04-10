0dfcc8efafc5c9504da7ca9250d4885f71fd0766
FarahIsmahHana <farahismahhana01@gmail.com>
2022-04-10 23:04:02 +0700

    menambahkan file form validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Tengah from "./StylingComponent-Reactjs/Card/Tengah";
-import Bawah from "./StylingComponent-Reactjs/Footer/Bawah";
-import Atas from "./StylingComponent-Reactjs/Header/Atas";
+import FormElement from "./FormValidation/FormElement";
+//import Tengah from "./StylingComponent-Reactjs/Card/Tengah";
+//import Bawah from "./StylingComponent-Reactjs/Footer/Bawah";
+//import Atas from "./StylingComponent-Reactjs/Header/Atas";
 
 const App = () => {
   return (
     <div>
-      <Atas />
-      <Tengah />
-      <Bawah />
+      <FormElement />
     </div>
   );
 };
